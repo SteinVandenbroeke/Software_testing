9f3c273b40c668b59bc02c9420b87609f277c1fa
Stein Vandenbroeke <stein.vandenbroeke@student.uantwerpen.be>
2025-06-30 16:06:00 +0200

    gitignore

---
+++ project1/JPacman/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jpacman.controller"},{"l":"jpacman.model"}];updateSearchResults();
---
+++ project1/JPacman/target/site/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jpacman"},{"l":"jpacman.controller"},{"l":"jpacman.model"}];updateSearchResults();
