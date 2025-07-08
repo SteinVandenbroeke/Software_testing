5eff884f6092d4377413c096bbce0ecebbc1e05d
Stein Vandenbroeke <stein.vandenbroeke@student.uantwerpen.be>
2025-07-08 13:46:47 +0200

    always new map

--- project7 fuzzing/fuzze_ex1/fuzzer.py
+++ project7 fuzzing/fuzze_ex1/fuzzer.py
@@ -1,8 +1,17 @@
 import argparse
+import datetime
 import os
 from random import randrange, randint
 import sys
 # Create the parser
+
+run = datetime.datetime.now()
+
+test_folder = f"generated_{run}"
+# run using python3 fuzzer.py --max 5
+if not os.path.isdir(test_folder):
+    os.mkdir(test_folder)
+
 parser = argparse.ArgumentParser(description="Fuzzer tool")
 
 # Add arguments
@@ -28,10 +37,10 @@ def generate_random_map():
 report = [None] * max_iterations
 
 for i in range(max_iterations):
-    os.mkdir(f"generated/test_{i}")
+    os.mkdir(f"{test_folder}/test_{i}")
     with open("input_map.txt", "a") as f:
         f.write(generate_random_map())
-    map_path = f"generated/test_{i}/input_map.txt"
+    map_path = f"{test_folder}/test_{i}/input_map.txt"
     random_sequence = generate_random_input_sequence()
     runcode = os.system(f'java -jar jpacman-3.0.1 jar {map_path} {random_sequence}')
     report[i] = runcode
